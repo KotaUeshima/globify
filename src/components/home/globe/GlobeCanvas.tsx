84e269183aace0eb2fece6a8cda6fe555d20b94f
Kota Ueshima <ueshima@usc.edu>
2023-06-10 09:59:46 -0500

    fix file error

--- src/components/home/globe/GlobeCanvas.tsx
+++ src/components/home/globe/GlobeCanvas.tsx
@@ -1,15 +1,14 @@
 import { Canvas } from '@react-three/fiber'
 import { Suspense } from 'react'
-import GlobeLoader from '../../loaders/GlobeLoader'
 import Globe from './Globe'
 
 function GlobeCanvas() {
   return (
-    // <Suspense fallback={<GlobeLoader />}>
-    <Canvas>
-      <Globe />
-    </Canvas>
-    // </Suspense>
+    <Suspense fallback={null}>
+      <Canvas>
+        <Globe />
+      </Canvas>
+    </Suspense>
   )
 }
 
